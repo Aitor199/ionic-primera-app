e81b3d4903940483b06f0175dc2061aee14992c5
Aitor <aitor.barreiro@tcomet.com>
2023-01-25 11:00:06 +0100

    Primeras modificaciones

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor() { }
+  ngOnInit() {
+    let numero = 10;
+    numero =20;
+    console.log(numero)
+  }
 }
